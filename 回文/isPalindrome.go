8a527a3f1bd9e75f17e7984964a1ae2a849700b7
jacket <1762982273@qq.com>
2020-01-05 21:54:57 +0800

    three of palindrome

--- 回文/isPalindrome.go
+++ 回文/isPalindrome.go
@@ -1,11 +1,10 @@
 package palindromic
 
+import "strconv"
+
 // IsPalindrome 回文函数
 func IsPalindrome(x int) bool {
-	if x == 0 {
-		return true
-	}
-	if x < 0 || x%10 == 0 {
+	if (x < 0) || (x%10 == 0 && x != 0) {
 		return false
 	}
 	var r, v int
@@ -15,4 +14,41 @@ func IsPalindrome(x int) bool {
 		r = r / 10
 	}
 	return (v/10 == r) || (v == r)
+}
+
+func IsPalindromeMath(x int) bool {
+	if (x < 0) || (x%10 == 0 && x != 0) {
+		return false
+	}
+	var div int = 1
+	for x/div >= 10 {
+		div *= 10
+	}
+	var left, right int
+	for x > 0 {
+		left = x / div
+		right = x % 10
+		if left != right {
+			return false
+		}
+		x = (x % div) / 10
+		div /= 100
+	}
+	return true
+}
+
+func IsPalindromeString(x int) bool {
+	if (x < 0) || (x%10 == 0 && x != 0) {
+		return false
+	}
+	var y string = strconv.Itoa(x)
+	var head, tail int
+	for head <= tail {
+		tail = len(y) - head - 1
+		if y[head] != y[tail] {
+			return false
+		}
+		head++
+	}
+	return true
 }
--- 回文/isPalindrome_test.go
+++ 回文/isPalindrome_test.go
@@ -24,3 +24,49 @@ func TestIsPalindrome(t *testing.T) {
 		t.Errorf("'%v' test faild", num5)
 	}
 }
+
+func TestIsPalindromeMath(t *testing.T) {
+	num1 := 1002
+	num2 := 10001
+	num3 := 0
+	num4 := 456
+	num5 := 1001001
+	if IsPalindromeMath(num1) != false {
+		t.Errorf("'%v' test faild", num1)
+	}
+	if IsPalindromeMath(num2) != true {
+		t.Errorf("'%v' test faild", num2)
+	}
+	if IsPalindromeMath(num3) != true {
+		t.Errorf("'%v' test faild", num2)
+	}
+	if IsPalindromeMath(num4) != false {
+		t.Errorf("'%v' test faild", num4)
+	}
+	if IsPalindromeMath(num5) != true {
+		t.Errorf("'%v' test faild", num5)
+	}
+}
+
+func TestIsPalindromeString(t *testing.T) {
+	num1 := 1002
+	num2 := 10001
+	num3 := 0
+	num4 := 456
+	num5 := 1001001
+	if IsPalindromeString(num1) != false {
+		t.Errorf("'%v' test faild", num1)
+	}
+	if IsPalindromeString(num2) != true {
+		t.Errorf("'%v' test faild", num2)
+	}
+	if IsPalindromeString(num3) != true {
+		t.Errorf("'%v' test faild", num2)
+	}
+	if IsPalindromeString(num4) != false {
+		t.Errorf("'%v' test faild", num4)
+	}
+	if IsPalindromeString(num5) != true {
+		t.Errorf("'%v' test faild", num5)
+	}
+}
