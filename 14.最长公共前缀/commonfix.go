5d08a53ba80ecaa00f2ae09376386371bfd9334f
jacket <1762982273@qq.com>
2020-01-07 23:00:51 +0800

    fix longest common prefix

--- 14.最长公共前缀/commonfix.go
+++ 14.最长公共前缀/commonfix.go
@@ -1,27 +1,16 @@
 package longestCommonPrefix
 
-import "unicode"
-
 func GetLongestCommonPrefix(strs []string) string {
 	if len(strs) == 0 || strs[0] == "" {
 		return ""
 	}
-	if len(strs) == 1 {
-		return strs[0]
-	}
-	var str = strs[0]
-	var i = 0
-	for i < len(str) {
-		if unicode.IsUpper(rune(str[i])) {
-			return str[0:i]
-		} else {
-			for _, v := range strs {
-				if v == "" || i >= len(v) || str[i] != v[i] {
-					return str[0:i]
-				}
+	str := strs[0]
+	for i := 0; i < len(str); i++ {
+		for j := 1; j < len(strs); j++ {
+			if i == len(strs[j]) || str[i] != strs[j][i] {
+				return str[0:i]
 			}
 		}
-		i++
 	}
-	return str[0:i]
+	return str
 }
